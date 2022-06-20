36cfd737667d9a912c9c26a9427ff0e27e77c8d3
Michael Birchmeier <mbirchmeier@gmail.com>
2022-06-20 10:21:15 -0700

    First pointer problem for go leetcode

--- go/Problems/Prob103.go
+++ go/Problems/Prob103.go
@@ -1,9 +1,22 @@
 package problems
 
+import "fmt"
+
 func isPowerOfThree(n int) bool {
 	var test int = 1
 	for test < n {
 		test *= 3
 	}
 	return test == n
+}
+
+func countPrimes(n int) int {
+	// var isPrime = make([]bool, n)
+
+	// for index, element := range isPrime {
+	fmt.Println(n)
+	// }
+
+	return 0
+
 }
--- go/hello.go
+++ go/hello.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+func reverseString(s []byte) []byte {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+	return s
+}
+
+//  Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list1 == nil && list2 == nil {
+		return nil
+	}
+	if list1 == nil {
+		return list2
+	}
+	if list2 == nil {
+		return list1
+	}
+
+	var toReturn ListNode
+	if list1.Val < list2.Val {
+		toReturn.Val = list1.Val
+		list1 = list1.Next
+	} else {
+		toReturn.Val = list2.Val
+		list2 = list2.Next
+	}
+	toReturn.Next = mergeTwoLists(list1, list2)
+
+	return &toReturn
+}
 
 func main() {
-	fmt.Println(problems.isPowerOfThree(27))
+	//fmt.Println(problem.countPrimes(27))
+	var l1, l2 ListNode
+	l1.Val = 1
+	l2.Val = 2
+
+	mergeTwoLists(&l1, &l2)
 }
